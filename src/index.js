aaf26f6147431eed5c8755148e3590c7f6c0356a
Pawan Kumar <pawan.kumar@squareboat.com>
2017-09-17 22:10:03 +0530

    Handling Images and Fonts

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+
+require('./vendors/bootstrap/css/bootstrap.css');
+require('./vendors/font-awesome/css/font-awesome.css');
 require('./sass/main.scss');
 
+require('./vendors/bootstrap/js/bootstrap.js');
 require('./js/main.js');
--- webpack.config.js
+++ webpack.config.js
@@ -7,22 +7,56 @@ module.exports = {
   },
   output: {
       path: path.resolve(__dirname, 'dist'),
+      publicPath : 'dist/',
       filename: "[name].js",
   },
   module: {
-    rules: [{
-      test: /\.scss$/,
-      use: [{
-          loader: "style-loader" // creates style nodes from JS strings
-      }, {
-          loader: "css-loader" // translates CSS into CommonJS
-      }, {
-          loader: "sass-loader" // compiles Sass to CSS
-      }]
-    }]
+    rules: [
+      {
+        test: /\.css$/, use : [{
+          loader : "style-loader"
+        }, {
+          loader : "css-loader"
+        }]
+      },
+      {
+        test: /\.scss$/,
+        use: [{
+            loader: "style-loader" // creates style nodes from JS strings
+        }, {
+            loader: "css-loader" // translates CSS into CommonJS
+        }, {
+            loader: "sass-loader" // compiles Sass to CSS
+        }]
+      },
+      {
+        test: /\.(png|jpg|gif)$/,
+        use: [
+          {
+            loader: 'url-loader',
+            options: {
+              limit: 8192,
+            }  
+          }
+        ]
+      },
+      {
+        test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
+        use: [
+          {
+            loader: 'file-loader'
+          }
+        ]
+      }
+    ]
   },
   plugins: [
-    new webpack.optimize.UglifyJsPlugin()
+    new webpack.ProvidePlugin({
+      $: "jquery",
+      jQuery: "jquery",
+      Tether: 'tether'
+    }),
+    // new webpack.optimize.UglifyJsPlugin()
   ],
   watch: true
 }
