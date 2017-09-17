0180e3725a16e542e7af9f1338a402de0da61a99
Pawan Kumar <pawan.kumar@squareboat.com>
2017-09-17 23:08:29 +0530

    Hot Module Replacement

--- src/index.js
+++ src/index.js
@@ -5,3 +5,25 @@ require('./sass/main.scss');
 
 require('./vendors/bootstrap/js/bootstrap.js');
 require('./js/main.js');
+
+if(module.hot){
+  module.hot.accept();
+}
+
+if (module.hot) {
+    const hotEmitter = require("webpack/hot/emitter");
+    const DEAD_CSS_TIMEOUT = 2000;
+
+    hotEmitter.on("webpackHotUpdate", function(currentHash) {
+        document.querySelectorAll("link[href][rel=stylesheet]").forEach((link) => {
+            const nextStyleHref = link.href.replace(/(\?\d+)?$/, `?${Date.now()}`);
+            const newLink = link.cloneNode();
+            newLink.href = nextStyleHref;
+
+            link.parentNode.appendChild(newLink);
+            setTimeout(() => {
+                link.parentNode.removeChild(link);
+            }, DEAD_CSS_TIMEOUT);
+        });
+    })
+}
--- webpack.config.js
+++ webpack.config.js
@@ -9,44 +9,56 @@ module.exports = {
     },
     output: {
         path: path.resolve(__dirname, 'dist'),
+        publicPath : '/',
         filename: "[name].js",
     },
     module: {
-        rules: [{
-            test: /\.css$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: "babel-loader"
+            },
+            {
+                test: /\.css$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader",
+                        options : { sourceMap : true }
+                    }]
+                })
+            }, 
+
+            {
+                test: /\.scss$/,
+                use: ExtractTextPlugin.extract({
+                    fallback: "style-loader",
+                    use: [{
+                        loader: "css-loader", // translates CSS into CommonJS
+                        options : { sourceMap : true }
+                    }, {
+                        loader: "sass-loader", // compiles Sass to CSS
+                        options : { sourceMap : true }
+                    }]
+                })
+            }, 
+            {
+                test: /\.(png|jpg|gif)$/,
                 use: [{
-                    loader: "css-loader",
-                    options : { sourceMap : true }
+                    loader: 'url-loader',
+                    options: {
+                        limit: 8192,
+                    }
                 }]
-            })
-        }, {
-            test: /\.scss$/,
-            use: ExtractTextPlugin.extract({
-                fallback: "style-loader",
+            }, 
+            {
+                test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
                 use: [{
-                    loader: "css-loader", // translates CSS into CommonJS
-                    options : { sourceMap : true }
-                }, {
-                    loader: "sass-loader", // compiles Sass to CSS
-                    options : { sourceMap : true }
+                    loader: 'file-loader'
                 }]
-            })
-        }, {
-            test: /\.(png|jpg|gif)$/,
-            use: [{
-                loader: 'url-loader',
-                options: {
-                    limit: 8192,
-                }
-            }]
-        }, {
-            test: /\.(eot|svg|ttf|woff|woff2|otf)$/,
-            use: [{
-                loader: 'file-loader'
-            }]
-        }]
+            }
+        ]
     },
     plugins: [
         new webpack.ProvidePlugin({
@@ -56,17 +68,30 @@ module.exports = {
         }),
         new ExtractTextPlugin("[name].css"),
         new webpack.optimize.UglifyJsPlugin(),
-        // new OptimizeCssAssetsPlugin({
-        //     assetNameRegExp: /\.css$/g,
-        //     cssProcessor: require('cssnano'),
-        //     cssProcessorOptions: {
-        //         discardComments: {
-        //             removeAll: true
-        //         }
-        //     },
-        //     canPrint: true
-        // })
+        new webpack.HotModuleReplacementPlugin(),
+        new webpack.NamedModulesPlugin(),
+        new OptimizeCssAssetsPlugin({
+            assetNameRegExp: /\.css$/g,
+            cssProcessor: require('cssnano'),
+            cssProcessorOptions: {
+                discardComments: {
+                    removeAll: true
+                }
+            },
+            canPrint: true
+        })
     ],
-    devtool: 'inline-source-map',
-    watch: true
+    devtool: 'cheap-module-eval-source-map',
+    watch: true,
+    devServer : {
+        hot : true,
+        inline: true,
+        port :  8080,
+        contentBase: path.resolve(__dirname, ''),
+        headers: {
+          "Access-Control-Allow-Origin": "*",
+          "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
+          "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
+        }
+    }
 }
